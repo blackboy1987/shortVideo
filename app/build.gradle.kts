48b192a7ee1f71cbb065361a73927bb042871a90
black <1169794338@qq.com>
2024-03-13 17:24:20 +0800

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer-dash:1.3.0")
     implementation("androidx.media3:media3-ui:1.3.0")
 
-    implementation("io.sanghun:compose-video:1.2.0")
     implementation("androidx.media3:media3-session:1.3.0")
 
     implementation("androidx.media3:media3-exoplayer-dash:1.3.0")
