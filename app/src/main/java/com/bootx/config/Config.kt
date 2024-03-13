8af47b1e1680a5504a8c2ba7feef06446c97668f
xial@plian.net <xial@plian.net>
2024-03-13 22:27:19 +0800

    init

--- app/src/main/java/com/bootx/config/Config.kt
+++ app/src/main/java/com/bootx/config/Config.kt
@@ -4,8 +4,8 @@ object Config {
 
     //var baseUrl = "http://yysc.igomall.xin"
     //var baseUrl = "https://965z2991e.oicp.vip"
-    //var baseUrl = "http://192.168.31.214:9991"
-    var baseUrl = "http://172.16.13.94:9991"
+    var baseUrl = "http://192.168.31.214:9991"
+    //var baseUrl = "http://172.16.13.94:9991"
     var messageTypeList = listOf("系统消息", "收到的点赞", "应用状态", "收到的投币", "好友关注")
     var imageUrl = "https://bootx-tuchuang.oss-cn-hangzhou.aliyuncs.com/"
     var initToken = ""
--- app/src/main/java/com/bootx/ui/screens/PlayScreen.kt
+++ app/src/main/java/com/bootx/ui/screens/PlayScreen.kt
@@ -1,15 +1,26 @@
 package com.bootx.ui.screens
 
 
+import android.app.Activity
 import android.util.Log
 import androidx.annotation.OptIn
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,43 +28,54 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.analytics.AnalyticsListener
 import androidx.navigation.NavHostController
-import com.bootx.util.SharedPreferencesUtils
-import com.bootx.viewmodel.PlayViewModel
 import com.bootx.compose.video.RepeatMode
 import com.bootx.compose.video.ResizeMode
+import com.bootx.compose.video.VideoPlayer
 import com.bootx.compose.video.controller.VideoPlayerControllerConfig
 import com.bootx.compose.video.uri.VideoPlayerMediaItem
-import com.bootx.compose.video.VideoPlayer
+import com.bootx.util.SharedPreferencesUtils
+import com.bootx.viewmodel.PlayViewModel
 import kotlinx.coroutines.launch
 
 
 @OptIn(UnstableApi::class)
 @Composable
-fun PlayScreen(navController: NavHostController,fsId: String,playViewModel: PlayViewModel = viewModel()) {
+fun PlayScreen(
+    navController: NavHostController,
+    fsId: String,
+    playViewModel: PlayViewModel = viewModel()
+) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
-    var playUrl by remember { mutableStateOf("") }
+    var playUrl by remember { mutableStateOf("https://bootx-video.oss-cn-hangzhou.aliyuncs.com/270925551843771.m3u8") }
     var nextFsId by remember { mutableStateOf(fsId) }
     LaunchedEffect(Unit) {
-        playUrl = SharedPreferencesUtils(context).get(fsId)
-        playViewModel.items(context,fsId)
+        //playUrl = SharedPreferencesUtils(context).get(fsId)
+        playViewModel.items(context, fsId)
 
     }
-    val configuration = LocalConfiguration.current
-    val density = LocalDensity.current
 
-    val screenWidth = configuration.screenWidthDp.dp
-    val screenHeight = configuration.screenHeightDp.dp
+    // 获取当前的Activity
+    val activity = (LocalContext.current as? Activity)
+    // 在Compose的SideEffect中设置沉浸式状态栏
+    SideEffect {
+        // 设置内容显示在状态栏和导航栏下方
+        activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, false) }
+        activity?.window?.statusBarColor = android.graphics.Color.TRANSPARENT
+    }
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
@@ -61,9 +83,10 @@ fun PlayScreen(navController: NavHostController,fsId: String,playViewModel: Play
             Box(){
                 VideoPlayer(
                     Modifier
-                        .fillMaxSize().align(Alignment.TopCenter)
+                        .fillMaxSize()
+                        .align(Alignment.TopCenter)
                         .clickable {
-                            Log.e("VideoPlayer click", "PlayScreen: aaa" )
+                            Log.e("VideoPlayer click", "PlayScreen: aaa")
                         },
                     mediaItems = listOf(
                         VideoPlayerMediaItem.NetworkMediaItem(
@@ -118,6 +141,30 @@ fun PlayScreen(navController: NavHostController,fsId: String,playViewModel: Play
                         })
                     },
                 )
+            }
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopStart)
+                .padding(start = 16.dp, top = 32.dp)) {
+               Text(text = "第一季")
+            }
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart)
+                .padding(start = 16.dp, bottom = 32.dp, end = 16.dp)) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(text = "A炼气十万年 98集")
+                    OutlinedButton( contentPadding = PaddingValues(all = 0.dp),
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.size(width = 60.dp, height = 30.dp)
+                    ) {
+                        Text(text = "选集", fontSize = 12.sp, modifier = Modifier.padding(0.dp))
+                    }
+                }
             }
         }
     }
